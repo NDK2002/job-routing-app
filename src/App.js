23fb05b00c7444af61b952571a3c723676a41f06
NDK <ndk1200@gmail.com>
2024-07-05 00:11:13 +0700

    complete basic app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import HomePage from "./pages/HomePage";
+import Layout from "./pages/Layout";
 
 function App() {
-  return <div>ABC</div>;
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<HomePage />} />
+        </Route>
+      </Routes>
+    </>
+  );
 }
 
 export default App;
